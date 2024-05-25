7e591afe8ea070b3740644d7d89ce3b113acf3b5
Amrendra K <vision2010@gmail.com>
2024-05-25 22:02:23 +0530

    Update add.py

--- add.py
+++ add.py
@@ -4,6 +4,6 @@ num2 = 6.3
 # Add two numbers
 sum = num1 + num2
 diff = num1 - num2
-# sum
+# sum and difference 
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
 print('The diff of {0} and {1} is {2}'.format(num1, num2, diff))
