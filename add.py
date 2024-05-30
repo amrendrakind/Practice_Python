e3055f53ca12597d5ed67b5c6f53f420a6951580
amrendrakind <vision2010@gmail.com>
2024-05-30 23:02:08 +0530

    divide two numbers

--- add.py
+++ add.py
@@ -5,7 +5,9 @@ num2 = 6.3
 sum = num1 + num2
 diff = num1 - num2
 product = num1*num2
+division = num1/num2
 # sum, difference and multiplication of variables 
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
 print('The diff of {0} and {1} is {2}'.format(num1, num2, diff))
 print('The multiplication of {0} and {1} is {2}'.format(num1, num2, product))
+print('The multiplication of {0} and {1} is {2}'.format(num1, num2, division))
