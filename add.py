684e907d083c861655adbce6487cae33dd4cde3c
amrendrakind <vision2010@gmail.com>
2024-05-31 23:09:19 +0530

    divide two numbers

--- add.py
+++ add.py
@@ -6,8 +6,10 @@ sum = num1 + num2
 diff = num1 - num2
 product = num1*num2
 division = num1/num2
+remiander = num1 % num2
 # sum, difference and multiplication of variables 
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
 print('The diff of {0} and {1} is {2}'.format(num1, num2, diff))
 print('The multiplication of {0} and {1} is {2}'.format(num1, num2, product))
-print('The multiplication of {0} and {1} is {2}'.format(num1, num2, division))
+print('The division of {0} and {1} is {2}'.format(num1, num2, division))
+print('The remiander of {0} and {1} is {2}'.format(num1, num2, remiander))
