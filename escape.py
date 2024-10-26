37018cb59e14856dee0c737b121cede8b34daf98
amrendrakind <vision2010@gmail.com>
2024-10-26 21:08:25 +0530

    escaping of character

--- escape.py
+++ escape.py
@@ -2,3 +2,8 @@ print('It\'s a beautiful day!')
 print("We are from \"IIT\" Madras")
 print('My name is Amrendra.\t\tI am from IITM')
 print('My name is Amrendra.\nI am from IITM')
+address = '''My Address is given below
+I am from Mumbai
+And you are from.
+This is multi line example of storing string in variable'''
+print(address)
