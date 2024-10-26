2043a1b9b6bc6b504adb4c9bc62ecde1675dc5c0
amrendrakind <vision2010@gmail.com>
2024-10-26 20:33:12 +0530

    String manipulation

---
+++ concat.py
@@ -0,0 +1,4 @@
+first_name = str(input("Enter your first name: "))
+last_name = str(input("Enter your last name: "))
+full_name = first_name+" "+last_name
+print("Your full name is: ", full_name)
---
+++ string.py
@@ -0,0 +1,5 @@
+s = "coffee"
+t = "tea"
+print(s[1:5])
+print(s*len(s))
+
