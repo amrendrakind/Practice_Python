bae6a05cbbc958619bdec3cc270889dcc7f66f26
amrendrakind <vision2010@gmail.com>
2024-10-28 05:42:30 +0530

    Grading of marks

--- grading.py
+++ grading.py
@@ -5,11 +5,11 @@ while (marks < 0 or marks > 100):
         print("Invalid input")
 if (marks >= 90):
     print("A")
-elif(marks >= 80 and marks <90):
+elif(marks >= 80):
     print("B")
-elif(marks >= 70 and marks < 80):
+elif(marks >= 70):
     print("C")
-elif(marks >= 60 and marks < 70):
+elif(marks >= 60):
     print("D")
-elif(marks < 60):
+else:
     print("E")
