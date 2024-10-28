76ec406687d38d18a8ec0423d684bb764552fc50
amrendrakind <vision2010@gmail.com>
2024-10-28 07:25:34 +0530

    Find factorial

--- factorial.py
+++ factorial.py
@@ -1,6 +1,10 @@
 n = int(input("Enter number: "))
 fact = 1
+num = n
 while (n > 0):
     fact *= n
     n -= 1
-print("Factorials of",n,"is",fact)
+if (n >= 0):
+    print("Factorials of",num,"is",fact)
+else:
+    print("Not defined")
