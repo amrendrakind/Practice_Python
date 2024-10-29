68fd19b854786a4ce640ba4ab1675c05b5c86d3c
amrendrakind <vision2010@gmail.com>
2024-10-30 05:17:11 +0530

    Formating

---
+++ num_format.py
@@ -0,0 +1,3 @@
+num = int(input("Enter a number "))
+for i in range(1, num+1):
+    print(f'{num} x {i} = {num*i}')
---
+++ pi.py
@@ -0,0 +1,4 @@
+pi = 22/7
+print(f'Value of PI = {pi}')
+print('Value of PI = {0}'.format(pi))
+print('Value of PI = %f' %(pi))
