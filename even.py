61133b7f1e4f7e976773ee6b06c8d9b650ffae7a
amrendrakind <vision2010@gmail.com>
2024-10-30 04:43:53 +0530

    For loop

---
+++ even.py
@@ -0,0 +1,5 @@
+n = int(input("Enter a number: "))
+for i in range(n):
+    if(i%2 == 0):
+        print(i)
+
---
+++ summation.py
@@ -0,0 +1,5 @@
+n = int(input("Enter the nth Number "))
+sum = 0
+for i in range(n+1):
+    sum += i
+print("Sum of first",n,"number is",sum)
