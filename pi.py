0f6127d8fe5318c2bc5fcb5f2b4bb13a7f8671f6
amrendrakind <vision2010@gmail.com>
2024-10-30 05:22:31 +0530

    Formating

--- pi.py
+++ pi.py
@@ -1,4 +1,4 @@
 pi = 22/7
 print(f'Value of PI = {pi:.2f}')
-print('Value of PI = {0:.2f}'.format(pi))
-print('Value of PI = %.2f' %(pi))
+print('Value of PI = {0:.3f}'.format(pi))
+print('Value of PI = %.4f' %(pi))
