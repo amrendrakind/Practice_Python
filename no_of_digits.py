aca7fc2010e0132d88b5092f1b4c7219dcbd0b5c
amrendrakind <vision2010@gmail.com>
2024-10-30 05:35:51 +0530

    string representation

--- no_of_digits.py
+++ no_of_digits.py
@@ -5,3 +5,9 @@ while (number > 9):
     number = number//10
     digits += 1
 print("Number of digits in",num, "is", digits )
+
+str_num = str(num)
+dig = 0
+for c in str_num:
+    dig += 1
+print("Number of digits in", num,"is",dig)
