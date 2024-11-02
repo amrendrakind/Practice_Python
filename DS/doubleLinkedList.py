7d7934a858e55aeaf268afb35477e00e5b9e6a73
amrendrakind <vision2010@gmail.com>
2024-11-02 21:52:27 +0530

    Double Linked list

--- DS/doubleLinkedList.py
+++ DS/doubleLinkedList.py
@@ -29,9 +29,9 @@ class DoublyLinkedList:
         current_head = self.head_node
     
         if current_head != None:
-            current_head.set_head_node(new_head)
-            new_head.set_next_node = current_head
-        
+            current_head.set_prev_node(new_head)
+            new_head.set_next_node(current_head)
+
         self.head_node = new_head
 
         if self.tail_node == None:
@@ -50,3 +50,82 @@ class DoublyLinkedList:
         if self.head_node == None:
             self.head_node = new_tail
     
+    def remove_head(self):
+        removed_head = self.head_node
+        if removed_head == None:
+            return None
+        
+        self.head_node = removed_head.get_next_node()
+
+        if self.head_node != None:
+            self.head_node.set_prev_node(None)
+        
+        if removed_head == self.tail_node:
+            self.remove_tail()
+        
+        return removed_head.get_value()
+
+    def remove_tail(self):
+        removed_tail = self.tail_node
+
+        if removed_tail == None:
+            return None
+        
+        self.tail_node = removed_tail.get_prev_node()
+
+        if self.tail_node != None:
+            self.tail_node.set_next_node(None)
+        
+        if removed_tail == self.head_node:
+            self.remove_head()
+        
+        return removed_tail.get_value()
+
+    def remove_by_value(self, value_to_remove):
+        node_to_remove = None
+        current_node = self.head_node
+
+        while current_node != None:
+            if current_node.get_value() == value_to_remove:
+                node_to_remove = current_node
+                break
+
+            current_node = current_node.get_next_node()
+
+        if node_to_remove == None:
+            return None
+
+        if node_to_remove == self.head_node:
+            self.remove_head()
+        elif node_to_remove == self.tail_node:
+            self.remove_tail()
+        else:
+            next_node = node_to_remove.get_next_node()
+            prev_node = node_to_remove.get_prev_node()
+            next_node.set_prev_node(prev_node)
+            prev_node.set_next_node(next_node)
+
+        return node_to_remove
+    
+    def stringify_list(self):
+        string_list = ""
+        current_node = self.head_node
+        while current_node:
+            if current_node.get_value() != None:
+                string_list += str(current_node.get_value()) + "\n"
+            current_node = current_node.get_next_node()
+        return string_list
+    
+ #Subway line here:
+subway = DoublyLinkedList()
+subway.add_to_head("Times Square")
+subway.add_to_head("Grand Central")
+subway.add_to_head("Central Park")
+print(subway.stringify_list())
+subway.add_to_tail("Penn Station")
+subway.add_to_tail("Wall Street")
+subway.add_to_tail("Brooklyn Bridge")
+print(subway.stringify_list())
+subway.remove_head()
+subway.remove_tail()
+print(subway.stringify_list())
