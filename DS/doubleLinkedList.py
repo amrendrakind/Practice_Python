ab70c6b6ed580e3cba2ee4f2fa27e32b38d9a1d6
amrendrakind <vision2010@gmail.com>
2024-11-02 14:21:51 +0530

    Double Linked list

--- DS/doubleLinkedList.py
+++ DS/doubleLinkedList.py
@@ -16,11 +16,37 @@ class Node:
     def get_prev_node(self):
         return self.prev_node
 
-    def get_value(self)
+    def get_value(self):
         return self.value
     
 class DoublyLinkedList:
     def __init__(self):
         self.head_node = None
         self.tail_node = None
+    
+    def add_to_head(self, new_value):
+        new_head = Node(new_value)
+        current_head = self.head_node
+    
+        if current_head != None:
+            current_head.set_head_node(new_head)
+            new_head.set_next_node = current_head
+        
+        self.head_node = new_head
+
+        if self.tail_node == None:
+            self.tail_node = new_head
+
+    def add_to_tail(self, new_value):
+        new_tail = Node(new_value)
+        current_tail = self.tail_node
+
+        if current_tail != None:
+            current_tail.set_next_node(new_tail)
+            new_tail.set_prev_node(current_tail)
+
+        self.tail_node = new_tail
+
+        if self.head_node == None:
+            self.head_node = new_tail
     
