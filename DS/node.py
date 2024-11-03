c1d032ca46a0a9de9d1880645e9d3e56f69dffe9
amrendrakind <vision2010@gmail.com>
2024-11-03 14:19:50 +0530

    Dequeue

--- DS/node.py
+++ DS/node.py
@@ -1,28 +1,14 @@
+# We'll be using our Node class
 class Node:
-  def __init__(self, value, link_node=None):
+  def __init__(self, value, next_node=None):
     self.value = value
-    self.link_node = link_node
+    self.next_node = next_node
     
-  def set_link_node(self, link_node):
-    self.link_node = link_node
-    
-  def get_link_node(self):
-    return self.link_node
-  
   def get_value(self):
     return self.value
-
-# Add your code below:
-# yacko = Node("likes to yak")
-# wacko = Node("has a penchant for hoarding snacks")
-# dot = Node("enjoys spending time in movie lots")
-
-# dot.set_link_node(wacko)
-# yacko.set_link_node(dot)
-
-# dots_data = yacko.get_link_node().get_value()
-# wackos_data = dot.get_link_node().get_value()
-
-# print(dots_data)
-# print(wackos_data)
-
+  
+  def get_next_node(self):
+    return self.next_node
+  
+  def set_next_node(self, next_node):
+    self.next_node = next_node
--- DS/queue.py
+++ DS/queue.py
@@ -50,10 +50,38 @@ class Queue:
             else:
                 self.head = self.head.get_next_node()
             self.size -= 1
+            return item_to_remove.get_value()
         else:
             print("This queue is totally empty!")
-        return item_to_remove.get_value()
         
-q = Queue()
-q.enqueue("some guy with a mustache")
-q.dequeue()
+print("Creating a deli line with up to 10 orders...\n------------")
+deli_line = Queue(10)
+print("Adding orders to our deli line...\n------------")
+deli_line.enqueue("egg and cheese on a roll")
+deli_line.enqueue("bacon, egg, and cheese on a roll")
+deli_line.enqueue("toasted sesame bagel with butter and jelly")
+deli_line.enqueue("toasted roll with butter")
+deli_line.enqueue("bacon, egg, and cheese on a plain bagel")
+deli_line.enqueue("two fried eggs with home fries and ketchup")
+deli_line.enqueue("egg and cheese on a roll with jalapeos")
+deli_line.enqueue("plain bagel with plain cream cheese")
+deli_line.enqueue("blueberry muffin toasted with butter")
+deli_line.enqueue("bacon, egg, and cheese on a roll")
+# ------------------------ #
+deli_line.enqueue("western omelet with home fries")
+# ------------------------ #
+print("------------\nOur first order will be " + deli_line.peek())
+print("------------\nNow serving...\n------------")
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+deli_line.dequeue()
+# ------------------------ #
+deli_line.dequeue()
+# ------------------------ #
