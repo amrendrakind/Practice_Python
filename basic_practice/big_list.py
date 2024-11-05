aaf2a4e82106615366239470d38104c505a1b59b
amrendrakind <vision2010@gmail.com>
2024-11-05 08:34:59 +0530

    search number in random list

--- basic_practice/big_list.py
+++ basic_practice/big_list.py
@@ -1,5 +1,14 @@
 import random
 l = []
+a = int(input("Enter a number "))
+flag = 0
 for i in range(100):
     l.append(random.randint(1,1000000))
 print(l)
+for i in range(len(l)):
+    if ( a == l[i] ):
+        flag = 1
+        print("Hip hip hurrey, Number found")
+        break
+if (flag == 0 ):
+    print("Number not found")
