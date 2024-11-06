db8d692b1705ee88d006a63362d6af4c95241350
amrendrakind <vision2010@gmail.com>
2024-11-06 12:13:48 +0530

    Addition update

--- basic_practice/.ipynb_checkpoints/add-checkpoint.py
+++ basic_practice/.ipynb_checkpoints/add-checkpoint.py
@@ -6,11 +6,11 @@ sum = num1 + num2
 diff = num1 - num2
 product = num1*num2
 division = num1/num2
-remainder = num1 % num2
+remiander = num1 % num2
 # sum, difference and multiplication, division and remainder of variables 
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
 print('The diff of {0} and {1} is {2}'.format(num1, num2, diff))
 print('The multiplication of {0} and {1} is {2}'.format(num1, num2, product))
 print('The division of {0} and {1} is {2}'.format(num1, num2, division))
-print('The remainder of {0} and {1} is {2}'.format(num1, num2, remainder))
-print('The remainder of {0} and {1} is {2}'.format(num1, num2, remainder))
+print('The remiander of {0} and {1} is {2}'.format(num1, num2, remiander))
+print('The remiander of {0} and {1} is {2}'.format(num1, num2, remiander))
