422ed3c04ec6424b485925beed21c32f962284b7
amrendrakind <vision2010@gmail.com>
2024-11-11 16:50:27 +0530

    Concatnating list item

--- basic_practice/concat.py
+++ basic_practice/concat.py
@@ -2,3 +2,11 @@ first_name = str(input("Enter your first name: "))
 last_name = str(input("Enter your last name: "))
 full_name = first_name+" "+last_name
 print("Your full name is: ", full_name)
+
+l = list(range(10))
+str_concat = ''
+for i in l:
+    str_concat = str_concat + str(i) + ' '
+print(len(str_concat))
+print(str_concat.strip())
+print(len(str_concat.strip()))
