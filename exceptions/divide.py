073de8f02b98ef5a4712552401f97414b3fc25c3
amrendrakind <vision2010@gmail.com>
2024-11-12 16:30:54 +0530

    Exception update

--- exceptions/divide.py
+++ exceptions/divide.py
@@ -1,9 +1,20 @@
 try:
     a = int(input("Enter a number A "))
     b = int(input("Enter a number B "))
+    f = open('abc.txt','r')
     c = a / b
-    print(f"A/B is ",c)
+    print("A/B is ",c)
 except ZeroDivisionError:
     print('Invalid input, divisor can not be zero.')
 except ValueError:
     print("Please input number. Non-numeric input is not allowed.")
+except FileNotFoundError:
+    print("File not found")
+except:
+    print("Something went wrong!")
+finally:
+    try:
+        f.close()
+    except NameError:
+        pass
+    print("From finally block")
