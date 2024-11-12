2a61fd4976bd7de263c016efb6507e70da94a8d0
amrendrakind <vision2010@gmail.com>
2024-11-12 15:04:15 +0530

    Exception update

--- exceptions/divide.py
+++ exceptions/divide.py
@@ -1,7 +1,9 @@
-a = int(input("Enter a number A "))
-b = int(input("Enter a number B "))
 try:
+    a = int(input("Enter a number A "))
+    b = int(input("Enter a number B "))
     c = a / b
     print(f"A/B is ",c)
 except ZeroDivisionError:
-    print('Invalid input, divisor can not be zero')
+    print('Invalid input, divisor can not be zero.')
+except ValueError:
+    print("Please input number. Non-numeric input is not allowed.")
