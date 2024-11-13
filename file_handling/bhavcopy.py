92e809b12df9fe5558396faf0736ae2a23582481
amrendrakind <vision2010@gmail.com>
2024-11-13 06:05:08 +0530

    Pandas using

---
+++ file_handling/bhavcopy.py
@@ -0,0 +1,7 @@
+# f = open('sec_bhavdata_full_12112024.csv', 'r')
+import pandas as pd
+cm = pd.read_csv('sec_bhavdata_full_12112024.csv')
+cm.columns = cm.columns.str.strip()
+cm = cm.applymap(lambda x: x.strip() if isinstance(x, str) else x)
+
+print(cm[cm['SERIES'] == 'BE'])
